a0389baa99f8e6123edd39861c56d9b7d8c9a678
Aleh Zaharau <38154177+AlehZakharau@users.noreply.github.com>
2023-10-03 18:23:55 -0400

    Add GeneratorUtility

---
+++ Assets/EnumGenerator/Editor/GeneratorUtility.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace UnityEditor.Generator
+{
+    public static class GeneratorUtility
+    {
+        private static List<string> PrepareEnumsForGenerator<T>(string enumType) where T : Enum
+        {
+            Array array = Enum.GetValues(typeof(T));
+            List<string> newEnumInstances = (from object enumInstance in array select enumInstance.ToString()).ToList();
+            newEnumInstances.Add(enumType);
+            return newEnumInstances;
+        }
+    }
+}
